4802f36116e47c2bf41e63f9e0cd49b7158ce515
alexDmello <alexdmelloc@gmail.com>
2026-07-20 00:13:13 +0530

    config: Setup Mellod PWA branding SVG icon and update manifest settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable}>
       <head>
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/icon.svg" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="mobile-web-app-capable" content="yes" />
       </head>
